16c02837e3e80fbb955074f10b0134de475f3d15
oskar-norberg <sus.mfringo@gmail.com>
2024-04-15 14:58:08 +0200

    Prevent loading same model twice

--- include/modelLoader.h
+++ include/modelLoader.h
@@ -13,6 +13,14 @@ using std::string;
 class ModelLoader {
 public:
 	Model* loadModel(std::string modelPath) {
+		// Prevents loading same model twice
+		for (size_t i = 0; i < loadedModels.size(); i++)
+		{
+			if (strcmp((*loadedModels[i]).getModelPath().c_str(), modelPath.c_str()) == 0) {
+				printf("Model already loaded.\n");
+				return loadedModels[i];
+			}
+		}
 		Model* model = new Model(modelPath);
 		loadedModels.push_back(model);
 		return model;
