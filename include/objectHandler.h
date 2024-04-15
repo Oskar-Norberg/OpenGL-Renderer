cb1e92dfa71caf97eaf18eec673b34bca0e5d467
oskar-norberg <sus.mfringo@gmail.com>
2024-04-15 16:21:25 +0200

    Create objects in ObjectHandler

---
+++ include/objectHandler.h
@@ -0,0 +1,29 @@
+#ifndef OBJECT_HANDLER_H
+#define OBJECT_HANDLER_H
+
+#include <stdio.h>
+#include <vector>
+#include <string>
+
+#include "shader.h"
+#include "model.h"
+#include "object.h"
+
+class ObjectHandler {
+public:
+	Object* createOpaqueObject(glm::vec3 position, Model* model, Shader* shader) {
+		Object* testObject = new Object(position, model, shader);
+		opaqueObjects.push_back(testObject);
+	}
+
+	Object* createTransparentObject(glm::vec3 position, Model* model, Shader* shader) {
+		Object* testObject = new Object(position, model, shader);
+		transparentObjects.push_back(testObject);
+	}
+
+private:
+	vector<Object*> opaqueObjects;
+	vector<Object*> transparentObjects;
+};
+
+#endif
