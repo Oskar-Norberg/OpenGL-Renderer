e209074a456e4b0f98cdcb099dc7e8dc932434cf
oskar-norberg <sus.mfringo@gmail.com>
2024-04-15 17:26:11 +0200

    Lights no longer extend object

--- include/pointLight.h
+++ include/pointLight.h
@@ -3,14 +3,17 @@
 
 #include "light.h"
 
-class PointLight : public Light, public Object{
+class PointLight : public Light{
 public:
-	PointLight(glm::vec3 position, Model* model, Shader* shader, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic)
-		: Light(ambient, diffuse, specular, constant, linear, quadratic),
-		Object(position, model, shader) {
-
+	PointLight(glm::vec3 position, Shader* shader, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic)
+		: Light(ambient, diffuse, specular, constant, linear, quadratic) {
+		this->position = position;
 	}
-
+	glm::vec3 getPosition() {
+		return this->getPosition;
+	}
+private:
+	glm::vec3 position;
 };
 
 #endif
--- include/spotlight.h
+++ include/spotlight.h
@@ -3,14 +3,18 @@
 
 #include "light.h"
 
-class Spotlight : public Light, public Object {
+class Spotlight : public Light {
 public:
-	Spotlight(glm::vec3 position, Model* model, Shader* shader, glm::vec3 direction, float cutoff, float outerCutoff, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic)
-		: Light(ambient, diffuse, specular, constant, linear, quadratic),
-		Object(position, model, shader) {
+	Spotlight(glm::vec3 position, glm::vec3 direction, float cutoff, float outerCutoff, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic)
+		: Light(ambient, diffuse, specular, constant, linear, quadratic) {
+		this->position = position;
 		this->direction = direction;
 		this->cutoff = glm::cos(glm::radians(cutoff));
 		this->outerCutoff = glm::cos(glm::radians(outerCutoff));
+	}
+
+	glm::vec3 getPosition() {
+		return this->position;
 	}
 
 	glm::vec3 getDirection() {
@@ -26,6 +30,7 @@ public:
 	}
 
 private:
+	glm::vec3 position;
 	glm::vec3 direction;
 	float cutoff, outerCutoff;
 };
