7abc214699d23a959fed519e9e5ea9cd96622514
oskar-norberg <sus.mfringo@gmail.com>
2024-04-15 17:40:04 +0200

    Methods for creating lights

--- include/scene.h
+++ include/scene.h
@@ -8,8 +8,10 @@
 #include <stdio.h>
 
 #include "shader.h"
-#include "object.h"
-#include "model.h"
+
+#include "shaderManager.h"
+#include "modelLoader.h"
+#include "objectHandler.h"
 
 #include "pointLight.h"
 #include "spotlight.h"
@@ -19,15 +21,36 @@ class Scene {
 public:
 	Scene(){
 	}
+
+	PointLight* createPointLight(glm::vec3 position, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic) {
+		PointLight* p = new PointLight(position, ambient, diffuse, specular, constant, linear, quadratic);
+		pointLights.push_back(p);
+		setLights();
+		return p;
+	}
+
+	Spotlight* createSpotlight(glm::vec3 position, glm::vec3 direction, float cutoff, float outerCutoff, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic) {
+		Spotlight* s = new Spotlight(position, direction, cutoff, outerCutoff, ambient, diffuse, specular, constant, linear, quadratic);
+		spotlights.push_back(s);
+		return s;
+	}
+
+	
+
 private:
-	vector<Object*> opaqueObjects;
-	vector<Object*> transparentObjects;
+	ShaderManager shaderManager;
+	ModelLoader modelLoader;
+	ObjectHandler objectHandler;
 
 	DirectionalLight sun;
 	vector<PointLight*> pointLights;
 	vector<Spotlight*> spotlights;
 
 	glm::vec4 backgroundColor;
+
+	void setLights() {
+		shaderManager.setLights(sun, pointLights, spotlights);
+	}
 };
 
 #endif
